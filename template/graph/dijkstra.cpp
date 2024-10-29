138574f39550ed125d4cfca511ad7ddc1da476c5
1664991608@qq.com <ebeb241615@gmail.com>
2024-10-29 23:30:27 +0800

    单源最短路(非负权图)

--- template/graph/dijkstra.cpp
+++ template/graph/dijkstra.cpp
@@ -34,3 +34,18 @@ void dijkstra(int n, int s) {
     }
   }
 }
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    int t=1;cin>>t;
+    while(t--){
+        solve();
+    }
+    system("pause");
+    return 0;
+}
