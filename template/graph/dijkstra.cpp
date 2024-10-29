0ad34ab323ab30ed5c50908db83199e8b85e0dd6
1664991608@qq.com <ebeb241615@gmail.com>
2024-10-30 00:10:06 +0800

    d

--- template/graph/dijkstra.cpp
+++ template/graph/dijkstra.cpp
@@ -4,12 +4,14 @@ using namespace std;
 constexpr int MAXN=1e5+10;
 
 struct edge {
-  int v, w;
+  int v,w;
 };
 
 struct node {
-  int dis, u;
-  bool operator>(const node& a) const { return dis > a.dis; }
+    int dis,u;
+    bool operator>(const node& a) const{ 
+        return dis>a.dis; 
+    }
 };
 
 vector<vector<edge>> e(MAXN);
@@ -17,22 +19,22 @@ int dis[MAXN], vis[MAXN];
 priority_queue<node, vector<node>, greater<node>> q;
 
 void dijkstra(int n, int s) {
-  memset(dis, 0x3f, (n + 1) * sizeof(int));
-  dis[s] = 0;
-  q.push({0, s});
-  while (!q.empty()) {
-    int u = q.top().u;
-    q.pop();
-    if (vis[u]) continue;
-    vis[u] = 1;
-    for (auto ed : e[u]) {
-      int v = ed.v, w = ed.w;
-      if (dis[v] > dis[u] + w) {
-        dis[v] = dis[u] + w;
-        q.push({dis[v], v});
-      }
+    memset(dis, 0x3f, (n + 1) * sizeof(int));
+    dis[s]=0;
+    q.push({0, s});
+    while(!q.empty()){
+        int u=q.top().u;
+        q.pop();
+        if(vis[u])continue;
+        vis[u] = 1;
+        for(auto ed:e[u]){
+            int v=ed.v,w=ed.w;
+            if(dis[v]>dis[u]+w){
+              dis[v]=dis[u]+w;
+              q.push({dis[v],v});
+            }
+        }
     }
-  }
 }
 
 void solve(){
