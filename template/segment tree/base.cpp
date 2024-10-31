0df661c12da2c575ca74a307e2d6c931092a38ee
1664991608@qq.com <ebeb241615@gmail.com>
2024-10-31 20:34:39 +0800

    线段树

---
+++ template/segment tree/base.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+constexpr int maxn=1e5+10;
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    int t=1;cin>>t;
+    while(t--){
+        solve();
+    }
+    system("pause");
+    return 0;
+}
